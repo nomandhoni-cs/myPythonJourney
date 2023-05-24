017d77191c6c52b8d0ece3f5b0c9f5c047462251
nomandhoni-cs <nomandhoni@yahoo.com>
2023-05-24 21:52:07 +0600

    Learned some basic of python

---
+++ print.py
@@ -0,0 +1,15 @@
+print("Hello Beautiful People in my Live Stream");
+x = 6;
+randomString = "I am gonna blow this machine"
+print(x, randomString)
+print(type(x), type(randomString))
+# Multiple value and multiple variable
+a, b, c = "apple", "maple", "kaple"
+print(c)
+# Inserting some variable in an empty array
+fruits = [] # This is called list
+xy = ("apple", "banana", "cherry") # This is called tuple 
+xx = {"name" : "John", "age" : 36} # This is called dictionary
+print(type(fruits))
+print(type(xy))
+print(type(xx))
